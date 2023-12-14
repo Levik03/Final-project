2c68849af57d2127d03e84663d9ef5b77549a2b6
Levik03 <146747300+Levik03@users.noreply.github.com>
2023-12-14 09:28:05 +0000

    images

---
+++ scripts/javascripts.js
@@ -0,0 +1,26 @@
+function poster3_mouseover() { document.getElementById("poster3").src="img/carp1.png";}
+//mouse out funtion to switch the image of carp thumnbnail to the name of project
+function poster3_mouseout() { document.getElementById("poster3").src="img/Poster3.png";
+}
+
+function flexweb_mouseover() { document.getElementById("flexweb").src="img/WCS.png";}
+//mouse out funtion to switch the image of coding sheets thumnbnail to the name of project
+function flexweb_mouseout() { document.getElementById("flexweb").src="img/flexweb.png";
+}
+
+function type1_mouseover() { document.getElementById("type1").src="img/typesystem.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type1_mouseout() { document.getElementById("type1").src="img/Typographic systems-01.png";
+}
+
+function type7_mouseover() { document.getElementById("type7").src="img/DTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type7_mouseout() { document.getElementById("type7").src="img/Typographic systems-07.png";
+}
+
+
+
+
+
+
+
