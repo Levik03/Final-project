16775e41139d2201728ade659ce5250a4b907f1d
Levik03 <146747300+Levik03@users.noreply.github.com>
2023-12-14 14:34:36 +0000

    styled nav

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,23 +1,51 @@
+//java script on index page
 function poster3_mouseover() { document.getElementById("poster3").src="img/carp1.png";}
 //mouse out funtion to switch the image of carp thumnbnail to the name of project
 function poster3_mouseout() { document.getElementById("poster3").src="img/Poster3.png";
 }
-
 function flexweb_mouseover() { document.getElementById("flexweb").src="img/WCS.png";}
 //mouse out funtion to switch the image of coding sheets thumnbnail to the name of project
 function flexweb_mouseout() { document.getElementById("flexweb").src="img/flexweb.png";
 }
+function typethumb_mouseover() { document.getElementById("typethumb").src="img/typesystem.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function typethumb_mouseout() { document.getElementById("typethumb").src="img/Typographic systems-01.png";
+}
 
-function type1_mouseover() { document.getElementById("type1").src="img/typesystem.png";}
+
+//java script on typography page
+function type1_mouseover() { document.getElementById("type1").src="img/ATS.png";}
 //mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
 function type1_mouseout() { document.getElementById("type1").src="img/Typographic systems-01.png";
 }
-
-function type7_mouseover() { document.getElementById("type7").src="img/DTS.png";}
+function type2_mouseover() { document.getElementById("type2").src="img/DTS.png";}
 //mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
-function type7_mouseout() { document.getElementById("type7").src="img/Typographic systems-07.png";
+function type2_mouseout() { document.getElementById("type2").src="img/Typographic systems-07.png";
 }
-
+function type3_mouseover() { document.getElementById("type3").src="img/bTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type3_mouseout() { document.getElementById("type3").src="img/Typographic systems-03.png";
+}
+function type4_mouseover() { document.getElementById("type4").src="img/GTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type4_mouseout() { document.getElementById("type4").src="img/Typographic systems-04.png";
+}
+function type5_mouseover() { document.getElementById("type5").src="img/RADTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type5_mouseout() { document.getElementById("type5").src="img/Typographic systems-02.png";
+}
+function type6_mouseover() { document.getElementById("type6").src="img/MTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type6_mouseout() { document.getElementById("type6").src="img/Typographic systems-06.png";
+}
+function type7_mouseover() { document.getElementById("type7").src="img/RANTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type7_mouseout() { document.getElementById("type7").src="img/Typographic systems-05.png";
+}
+function type8_mouseover() { document.getElementById("type8").src="img/TTS.png";}
+//mouse out funtion to switch the image of typographic systems thumnbnail to the name of project
+function type8_mouseout() { document.getElementById("type8").src="img/Typographic systems-08.png";
+}
 
 
 
